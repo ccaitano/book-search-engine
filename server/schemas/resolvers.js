d8af546977984a8a187d4f8befc8fca463fdfeac
ccaitano <cheryl.caitano@gmail.com>
2022-10-29 09:47:50 -1000

    Update resolvers.js file with Query and Mutation data

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,16 +2,36 @@ const { Book, User } = require('../models');
 
 const resolvers = {
     Query: {
-        book: async () => {
-            return Book.find({});
-        },
         user: async (parent, {_id}) => {
             const params = _id ? { _id }: {};
             return User.find(params);
         },
     },
     Mutation: {
-        
+        createUser: async (parent, args) => {
+            const user = await User.create(args);
+            return user;
+        },
+        saveBook: async (parent, args, context) => {
+            if(context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id},
+                    { $addToSet: { savedBooks: book }},
+                    { new: true }
+                )
+                return updatedUser;
+            }
+        },
+        deleteBook: async(parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: {savedBooks: { bookId: bookId }}},
+                    { new: true }
+                )
+                return updatedUser;
+            }
+        },
     },
 }
 module.exports = resolvers;
